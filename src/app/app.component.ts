6eb2aca7bc47c5ac02ed2c97236905758995c7c8
injin0301 <injinkim0808@gmail.com>
2025-04-28 10:56:01 +0200

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../pages/home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'newLinux';
+
 }
